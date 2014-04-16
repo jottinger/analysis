25f2e4a5825d46e4125dddc333980f419dc1a8ab
jottinger <joeo@enigmastation.com>
2014-04-16 10:41:25 -0400

    Adding models, updating to 3.3.1 of Stanford NLP

--- core/src/main/java/com/redhat/osas/ml/analysis/SentimentAnalyzer.java
+++ core/src/main/java/com/redhat/osas/ml/analysis/SentimentAnalyzer.java
@@ -3,10 +3,10 @@ package com.redhat.osas.ml.analysis;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.process.DocumentPreprocessor;
-import edu.stanford.nlp.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.trees.Tree;
@@ -14,21 +14,49 @@ import edu.stanford.nlp.util.CoreMap;
 
 import java.io.StringReader;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SentimentAnalyzer {
-    public boolean hasSubject(String corpus, String text) {
-        MaxentTagger tagger = new MaxentTagger("models/wsj-0-18-bidirectional-nodistsim.tagger");
-        StringReader reader = new StringReader(corpus);
-        DocumentPreprocessor preprocessor = new DocumentPreprocessor(reader);
-        for (List<? extends HasWord> sentence : preprocessor) {
+    final MaxentTagger tagger;
+    final StanfordCoreNLP pipeline;
+    final Map<String, String> uppercaseWord = new ConcurrentHashMap<>();
 
-            // we're looking for text as a noun in the corpus.
-            List<TaggedWord> words = tagger.tagSentence(sentence);
-            System.out.println(words);
-            for (TaggedWord w : words) {
-                if (w.word().equalsIgnoreCase(text) && w.tag().startsWith("NN")) {
-                    return true;
+    public SentimentAnalyzer() {
+        tagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+        pipeline = new StanfordCoreNLP(props);
+    }
+
+    public boolean hasSubject(String corpus, String text) {
+        Pattern pattern=Pattern.compile(text, Pattern.CASE_INSENSITIVE);
+        Matcher matcher=pattern.matcher(corpus);
+        if(matcher.find()) {
+            String normalizedText = null;
+            String normalizedKey = text.toLowerCase();
+
+            if (!uppercaseWord.containsKey(normalizedKey)) {
+                uppercaseWord.put(normalizedKey, Character.toUpperCase(text.charAt(0)) + normalizedKey.substring(1));
+            }
+            normalizedText = uppercaseWord.get(text.toLowerCase());
+
+            corpus=matcher.replaceAll(normalizedText);
+
+            StringReader reader = new StringReader(corpus);
+            DocumentPreprocessor preprocessor = new DocumentPreprocessor(reader);
+            for (List<? extends HasWord> sentence : preprocessor) {
+                // we're looking for text as a noun in the corpus.
+                List<TaggedWord> words = tagger.tagSentence(sentence);
+                System.out.println(corpus);
+                System.out.println(words);
+                for (TaggedWord w : words) {
+                    if (w.word().equalsIgnoreCase(text) && w.tag().startsWith("NN")) {
+                        return true;
+                    }
                 }
             }
         }
@@ -36,9 +64,6 @@ public class SentimentAnalyzer {
     }
 
     int findSentiment(String text) {
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
         int mainSentiment = 0;
         if (text != null && text.length() > 0) {
             int longest = 0;
@@ -51,7 +76,6 @@ public class SentimentAnalyzer {
                     mainSentiment = sentiment;
                     longest = partText.length();
                 }
-
             }
         }
         return mainSentiment;
